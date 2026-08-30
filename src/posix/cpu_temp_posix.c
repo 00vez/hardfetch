a2773d7c6dce1712102de803348d94a74b2118dd
00vez <00vez>
2026-08-30 10:50:39 +0200

    v0.2.2: AppleSMC temp, RAM freq, clean

--- src/posix/cpu_temp_posix.c
+++ src/posix/cpu_temp_posix.c
@@ -2,17 +2,38 @@
 #include "../output.h"
 
 #include <stdio.h>
+#if defined(__APPLE__)
+#include <IOKit/IOKitLib.h>
+#include <CoreFoundation/CoreFoundation.h>
+#endif
 
 void print_cpu_temp_power(double load)
 {
     char detail[128];
     int tempC = -1;
+#if defined(__APPLE__)
+    io_service_t smc = IOServiceGetMatchingService(0, IOServiceNameMatching("AppleSMC"));
+    if (smc) {
+        CFStringRef key = CFStringCreateWithCString(NULL, "TC0D", kCFStringEncodingUTF8);
+        CFDataRef d = (CFDataRef)IORegistryEntryCreateCFProperty(smc, key, kCFAllocatorDefault, 0);
+        if (d && CFGetTypeID(d) == CFDataGetTypeID()) {
+            uint8_t* p = (uint8_t*)CFDataGetBytePtr(d);
+            CFIndex n = CFDataGetLength(d);
+            if (n == 2 && p[0] == 0 && p[1] == 0) { /* valid */ }
+            else if (n >= 1) tempC = (int)(int8_t)p[n-1];
+        }
+        if (d) CFRelease(d);
+        CFRelease(key);
+        IOObjectRelease(smc);
+    }
+#else
     FILE* f = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
     if (f) {
         int milli = 0;
         if (fscanf(f, "%d", &milli) == 1) tempC = milli / 1000;
         fclose(f);
     }
+#endif
 
     int pos = snprintf(detail, sizeof(detail), "Load  %.0f%%", load);
     if (tempC >= 0)
--- src/posix/memory_spd_posix.c
+++ src/posix/memory_spd_posix.c
@@ -1,8 +1,16 @@
+#include <sys/types.h>
+#include <sys/sysctl.h>
 #include "../memory_spd.h"
 
 int get_memory_speed(unsigned int* outSpeed, unsigned int* outCAS)
 {
     if (outSpeed) *outSpeed = 0;
     if (outCAS)   *outCAS = 0;
+    int mhz = 0;
+    size_t len = sizeof(mhz);
+    if (sysctlbyname("hw.memfrequency", &mhz, &len, NULL, 0) == 0 && mhz > 0) {
+        if (outSpeed) *outSpeed = (unsigned int)mhz;
+        return 0;
+    }
     return -1;
 }
