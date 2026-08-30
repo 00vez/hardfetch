63dd1eeb1f4e3938cbdab427738179862b77142e
00vez <00vez>
2026-08-30 09:54:10 +0200

    macOS: typedef u_int/u_char/u_short

--- src/posix/kernel_posix.c
+++ src/posix/kernel_posix.c
@@ -6,6 +6,9 @@
 #include <sys/utsname.h>
 
 #if defined(__APPLE__)
+typedef unsigned int u_int;
+typedef unsigned char u_char;
+typedef unsigned short u_short;
 #include <sys/types.h>
 #include <machine/types.h>
 #include <sys/sysctl.h>
